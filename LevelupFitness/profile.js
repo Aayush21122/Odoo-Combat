d128f28fffe8fff42c27be0656be49ad5db5902e
Rishabh2004 <panchalrishabh04@gmail.com>
2024-06-29 13:55:47 +0530

    backend 2.0

--- LevelupFitness/profile.js
+++ LevelupFitness/profile.js
@@ -1,28 +1,27 @@
 function goToGenderPage() {
-    window.location.href = 'gender.html';
-  }
+  window.location.href = 'gender.html';
+}
 
-  function selectGender(gender) {
-    console.log("Selected gender: " + gender);
+function selectGender(gender) {
+  console.log("Selected gender: " + gender);
+}
 
-  }
+function goToMeasureStep() {
+  window.location.href = 'measurement.html';
+}
+function goToFitnessStep() {
+  window.location.href = 'fitness.html';
+}
 
-  function goToMeasureStep() {
-    window.location.href = 'measurement.html';
-  }
-  function goToFitnessStep() {
-    window.location.href = 'fitness.html';
-  }
+document.getElementById('measurementsForm').addEventListener('submit', function (event) {
+  event.preventDefault();
 
-  document.getElementById('measurementsForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
+  const age = document.getElementById('age').value;
+  const weight = document.getElementById('weight').value;
+  const height = document.getElementById('height').value;
 
-    const age = document.getElementById('age').value;
-    const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
+  console.log('Age:', age);
+  console.log('Weight:', weight);
+  console.log('Height:', height);
 
-    console.log('Age:', age);
-    console.log('Weight:', weight);
-    console.log('Height:', height);
-    
-  });
+});
