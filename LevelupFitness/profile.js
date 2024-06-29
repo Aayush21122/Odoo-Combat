401b13c6b9d64e1283804e7330db2bb0b20ba96a
Uttu2204 <159681392+utkarshpatel2204@users.noreply.github.com>
2024-06-29 16:00:10 +0530

    add fitness

--- LevelupFitness/profile.js
+++ LevelupFitness/profile.js
@@ -1,28 +1,97 @@
 function goToGenderPage() {
-    window.location.href = 'gender.html';
+  window.location.href = 'gender.html';
+ 
+}
+var Gender
+var Age
+var Height
+var Weight
+var Fitness
+function selectGender(gender) {
+  console.log("Selected gender: " + gender);
+  Gender=gender
+  // male = document.getElementById('male')
+  // female = document.getElementById('female')
+  // // const element = document.getElementById()
+  // if (male.classList.contains('active')){
+  //   male.classList.remove('active')
+  //   female.classList.add('active')
+  // }
+  // else{
+  //   female.classList.remove('active')
+  //   male.classList.add('active')
+  // }
+  
+ 
+}
+
+function goToMeasureStep() {
+  window.location.href = 'measurement.html';
+}
+function goToFitnessStep() {
+  window.location.href = 'fitness.html';
+}
+
+document.getElementById('measurementsForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const age = document.getElementById('age').value;
+  const weight = document.getElementById('weight').value;
+  const height = document.getElementById('height').value;
+
+  console.log('Age:', age);
+  console.log('Weight:', weight);
+  console.log('Height:', height);
+  Age=age
+  Weight=weight
+  Height=height
+});
+
+
+function selectFitness(fitness) {
+  console.log("Selected gender: " + fitness);
+}
+function goToUsermodel() {
+  window.location.href = 'usermodel.html';
+}
+
+function goToTimePeriod() {
+  window.location.href = 'timeperiod.html';
+}
+
+
+document.getElementById('experienceForm').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  
+  const experience = document.querySelector('input[name="gymExperience"]:checked').value;
+
+  
+  if (experience === 'yes') {
+    window.location.href = 'timeperiod.html';
+  } else {
+    window.location.href = 'profile.html';
   }
+});
 
-  function selectGender(gender) {
-    console.log("Selected gender: " + gender);
 
-  }
+document.getElementById('timePeriodForm').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  
+  const months = document.getElementById('months').value;
 
-  function goToMeasureStep() {
-    window.location.href = 'measurement.html';
-  }
-  function goToFitnessStep() {
-    window.location.href = 'fitness.html';
-  }
+  
+  console.log('Number of months:', months);
 
-  document.getElementById('measurementsForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
+  
+  window.location.href = 'profile.html';
+});
 
-    const age = document.getElementById('age').value;
-    const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
-
-    console.log('Age:', age);
-    console.log('Weight:', weight);
-    console.log('Height:', height);
-    
-  });
+const measurement={
+  'gender':Gender,
+  'age':Age,
+  'weight':Weight,
+  'height':Height,
+  'fitness':Fitness
+}
+module.export=measurement
